c46d809e52567a8f63be935a00aca8d703f02e2b
Eric Deandrea <eric.deandrea@gmail.com>
2022-03-10 14:38:27 -0500

    Add greeting

--- src/main/java/org/acme/externalconfig/rest/HelloController.java
+++ src/main/java/org/acme/externalconfig/rest/HelloController.java
@@ -4,16 +4,18 @@ import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+	@Value("${hello.greeting}")
+	private String greeting;
 
 	@GetMapping(path = { "/hello", "/hello/{name}" }, produces = TEXT_PLAIN_VALUE)
 	public String hello(@PathVariable(required = false) Optional<String> name) {
-		return "Hello " + name.orElse("World");
+		return this.greeting + " " + name.orElse("World");
 	}
 }
